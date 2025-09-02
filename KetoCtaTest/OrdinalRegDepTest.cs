04a766bf580482fccf760fd8966b66675ecfc11c
Jill England <jill.england@comcast.net>
2025-09-01 20:08:02 -0700

    More Ordinal testing ... Now I need to implement it and run a few thousand regressions with it.

---
+++ KetoCtaTest/OrdinalRegDepTest.cs
@@ -0,0 +1,207 @@
+﻿using DataMiner;
+using LinearRegression;
+using Xunit.Abstractions;
+
+namespace KetoCtaTest
+{
+    public class OrdinalRegDepTest(ITestOutputHelper testOutputHelper)
+    {
+        [Fact]
+        public void ConvertOrdinalTest()
+        {
+            // Arrange
+            const string path = "TestData/keto-cta-quant-and-semi-quant.csv";
+            var goldMiner = new GoldMiner(path);
+
+            const string title = "DNcpv vs. OrdDesc";
+            var selector = new CreateSelector(title);
+            var cor = new ConvertOrdinal(selector, goldMiner.Eta);
+
+            // Act 1
+            Assert.True(cor.IsRegOrd);
+            Assert.False(cor.IsDepOrd);
+            Assert.False(cor.IsAsc);
+
+
+            foreach (var (id, x, y) in cor.RawDataPoints)
+            {
+                testOutputHelper.WriteLine($"sel- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            testOutputHelper.WriteLine("");
+            foreach (var (id, x, y) in cor.DataPoints)
+            {
+                testOutputHelper.WriteLine($"new- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            Assert.NotEqual(0, cor.DataPoints[^1].y);
+            Assert.Equal(6.3, cor.DataPoints[^1].y, 0.300);
+
+            for (var x = 0; x < cor.DataPoints.Length - 1; x++) Assert.Equal(x + 1, cor.DataPoints[x].x);
+
+            // Assert
+            var regression = new RegressionPvalue(cor.DataPoints.ToList());
+
+            // Act 2
+            Assert.NotNull(selector);
+            Assert.NotNull(regression);
+            Assert.Equal(0.3, regression.PValue, 0.32);
+            testOutputHelper.WriteLine($"Regression: {regression}");
+        }
+
+        [Fact]
+        public void ConvertOrdinalDepTest()
+        {
+            // Arrange
+            const string path = "TestData/keto-cta-quant-and-semi-quant.csv";
+            var goldMiner = new GoldMiner(path);
+
+            const string title = "OrdDesc vs. DNcpv";
+            var selector = new CreateSelector(title);
+            var cor = new ConvertOrdinal(selector, goldMiner.Eta);
+
+            // Act 1
+            Assert.True(cor.IsDepOrd);
+            Assert.False(cor.IsRegOrd);
+            Assert.False(cor.IsAsc);
+
+
+            foreach (var (id, x, y) in cor.RawDataPoints)
+            {
+                testOutputHelper.WriteLine($"sel- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            testOutputHelper.WriteLine("");
+            foreach (var (id, x, y) in cor.DataPoints)
+            {
+                testOutputHelper.WriteLine($"new- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            Assert.NotEqual(0, cor.DataPoints[^1].x);
+            Assert.Equal(6.3, cor.DataPoints[^1].x, 0.300);
+
+            for (var x = 0; x < cor.DataPoints.Length - 1; x++) Assert.Equal(x + 1, cor.DataPoints[x].y);
+
+            // Assert
+            var regression = new RegressionPvalue(cor.DataPoints.ToList());
+
+            // Act 2
+            Assert.NotNull(selector);
+            Assert.NotNull(regression);
+            Assert.Equal(0.3, regression.PValue, 0.32);
+            testOutputHelper.WriteLine($"Regression: {regression}");
+        }
+
+        [Fact]
+        public void SimpleDataSetRegOrdAscLookup()
+        {
+            // Arrange
+            const string path = "TestData/keto-cta-quant-and-semi-quant.csv";
+            var goldMiner = new GoldMiner(path);
+
+            const string title = "DCac vs. OrdAsc";
+            var selector = new CreateSelector(title);
+            var selResult = goldMiner.Zeta.Select(selector.Selector);
+
+            // Act 1
+            Assert.Equal(Token.OrdAsc, selector.RegressorCompile.token);
+            Assert.NotEqual(Token.OrdAsc, selector.DependentCompile.token);
+            Assert.NotEqual(Token.OrdDesc, selector.DependentCompile.token);
+            Assert.Equal(Token.Element, selector.DependentCompile.token);
+
+            var enumerable = selResult as (string id, double x, double y)[] ?? selResult.ToArray();
+            var valueTuples = selResult as (string id, double x, double y)[] ?? enumerable.ToArray();
+
+            Assert.Equal(0, valueTuples[0].y);
+
+            var newTuples = valueTuples.OrderBy(t => t.y).Select((t, index) => (t.id, x: (double)(index + 1), t.y))
+                .ToArray();
+            foreach (var (id, x, y) in valueTuples)
+            {
+                testOutputHelper.WriteLine($"sel- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            testOutputHelper.WriteLine("");
+            foreach (var (id, x, y) in newTuples)
+            {
+                testOutputHelper.WriteLine($"new- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            Assert.NotEqual(0, newTuples[0].y);
+            Assert.Equal(-19, newTuples[0].y);
+
+            for (var x = 0; x < newTuples.Length - 1; x++)
+                Assert.Equal(x + 1, newTuples[x].x);
+
+            var xyList = newTuples
+                .Select(tuple => (tuple.x, tuple.y))
+                .ToList();
+
+            // Assert
+            var regression = new RegressionPvalue(xyList);
+
+            // Act 2
+            Assert.NotNull(selResult);
+            Assert.NotEmpty(xyList);
+            Assert.NotNull(regression);
+            Assert.Equal(0.3275, regression.PValue, 0.32);
+            testOutputHelper.WriteLine($"Regression: {regression}");
+        }
+
+        [Fact]
+        public void SimpleDataSetRegOrdDescLookup()
+        {
+            // Arrange
+            const string path = "TestData/keto-cta-quant-and-semi-quant.csv";
+            var goldMiner = new GoldMiner(path);
+
+            const string title = "DNcpv vs. OrdDesc";
+            var selector = new CreateSelector(title);
+            var selResult = goldMiner.Eta.Select(selector.Selector);
+
+            // Act 1
+            Assert.Equal(Token.OrdDesc, selector.RegressorCompile.token);
+            Assert.NotEqual(Token.OrdDesc, selector.DependentCompile.token);
+            Assert.NotEqual(Token.OrdAsc, selector.DependentCompile.token);
+            Assert.Equal(Token.Element, selector.DependentCompile.token);
+
+            var enumerable = selResult as (string id, double x, double y)[] ?? selResult.ToArray();
+            var valueTuples = selResult as (string id, double x, double y)[] ?? enumerable.ToArray();
+
+            Assert.NotEqual(0, valueTuples[0].y);
+
+            var newTuples = valueTuples.OrderByDescending(t => t.y)
+                .Select((t, index) => (t.id, x: (double)(index + 1), t.y))
+                .ToArray();
+
+            foreach (var (id, x, y) in valueTuples)
+            {
+                testOutputHelper.WriteLine($"sel- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            testOutputHelper.WriteLine("");
+            foreach (var (id, x, y) in newTuples)
+            {
+                testOutputHelper.WriteLine($"new- Element ID: {id}, X: {x}, Y: {y}");
+            }
+
+            Assert.NotEqual(0, newTuples[^1].y);
+            Assert.Equal(6.3, newTuples[^1].y, 6.2);
+
+            for (var x = 0; x < newTuples.Length - 1; x++) Assert.Equal(x + 1, newTuples[x].x);
+
+            var xyList = newTuples.Select(tuple => (tuple.x, tuple.y)).ToList();
+
+            // Assert
+            var regression = new RegressionPvalue(xyList);
+
+            // Act 2
+            Assert.NotNull(selResult);
+            Assert.NotEmpty(xyList);
+            Assert.NotNull(regression);
+            Assert.Equal(0.3, regression.PValue, 0.32);
+            testOutputHelper.WriteLine($"Regression: {regression}");
+        }
+
+    }
+}
--- KetoCtaTest/SelectorTwoTest.cs
+++ KetoCtaTest/SelectorTwoTest.cs
@@ -44,7 +44,8 @@ namespace KetoCtaTest
                     testOutputHelper.WriteLine($"Normalized attribute: {attLn} -> {normalized}");
                     var compiled = Compile.Build(normalized);
                     // look up the expected normalized attLn
-                    var reflectValue = (double)(MazeTwistyPassages.GetNestedPropertyValue(element, compiled.numerator) ?? double.NaN);
+                    var reflectValue =
+                        (double)(MazeTwistyPassages.GetNestedPropertyValue(element, compiled.numerator) ?? double.NaN);
                     testOutputHelper.WriteLine($"\tCompiled   attribute: {attLn} -> {compiled} -> {reflectValue:F2}");
 
                 }
@@ -173,7 +174,8 @@ namespace KetoCtaTest
             var selector = new CreateSelector(title);
 
             // Validate
-            testOutputHelper.WriteLine($"Title: {title};   Dep: {selector.DependentCompile} vs Reg: {selector.RegressorCompile}");
+            testOutputHelper.WriteLine(
+                $"Title: {title};   Dep: {selector.DependentCompile} vs Reg: {selector.RegressorCompile}");
             Assert.Equal("DCac", selector.RegressorCompile.numerator);
             Assert.Equal("DTcpv", selector.RegressorCompile.denominator);
         }
@@ -182,7 +184,8 @@ namespace KetoCtaTest
         public void CompareReflectionGetPropertyMethods()
         {
             // Arrange
-            var miner = new GoldMiner("TestData/keto-cta-quant-and-semi-quant.csv"); // Ensure this file exists and is valid
+            var miner = new GoldMiner(
+                "TestData/keto-cta-quant-and-semi-quant.csv"); // Ensure this file exists and is valid
             var selector = new CreateSelector("Cac1/LnNcpv1 vs Cac0");
             var reg = selector.RegressorCompile;
             var dep = selector.DependentCompile;
@@ -212,158 +215,5 @@ namespace KetoCtaTest
             Assert.Equal("Visits[0].Cac", selector.RegressorCompile.numerator);
         }
 
-        [Fact]
-        public void SimpleDataSetRegOrdAscLookup()
-        {
-            // Arrange
-            const string path = "TestData/keto-cta-quant-and-semi-quant.csv";
-            var goldMiner = new GoldMiner(path);
-
-            const string title = "DCac vs. OrdAsc";
-            var selector = new CreateSelector(title);
-            var selResult = goldMiner.Zeta.Select(selector.Selector);
-
-            // Act 1
-            Assert.Equal(Token.OrdAsc, selector.RegressorCompile.token);
-            Assert.NotEqual(Token.OrdAsc, selector.DependentCompile.token);
-            Assert.NotEqual(Token.OrdDesc, selector.DependentCompile.token);
-            Assert.Equal(Token.Element, selector.DependentCompile.token);
-
-            var enumerable = selResult as (string id, double x, double y)[] ?? selResult.ToArray();
-            var valueTuples = selResult as (string id, double x, double y)[] ?? enumerable.ToArray();
-
-            Assert.Equal(0, valueTuples[0].y);
-
-            var newTuples = valueTuples.OrderBy(t => t.y).Select((t, index) => (t.id, x: (double)(index + 1), t.y)).ToArray();
-            foreach (var (id, x, y) in valueTuples)
-            {
-                testOutputHelper.WriteLine($"sel- Element ID: {id}, X: {x}, Y: {y}");
-            }
-            testOutputHelper.WriteLine("");
-            foreach (var (id, x, y) in newTuples)
-            {
-                testOutputHelper.WriteLine($"new- Element ID: {id}, X: {x}, Y: {y}");
-            }
-
-            Assert.NotEqual(0, newTuples[0].y);
-            Assert.Equal(-19, newTuples[0].y);
-
-            for (var x = 0; x < newTuples.Length - 1; x++)
-                Assert.Equal(x + 1, newTuples[x].x);
-
-            var xyList = newTuples
-                .Select(tuple => (tuple.x, tuple.y))
-                .ToList();
-
-            // Assert
-            var regression = new RegressionPvalue(xyList);
-
-            // Act 2
-            Assert.NotNull(selResult);
-            Assert.NotEmpty(xyList);
-            Assert.NotNull(regression);
-            Assert.Equal(0.3275, regression.PValue, 0.32);
-            testOutputHelper.WriteLine($"Regression: {regression}");
-        }
-
-        [Fact]
-        public void SimpleDataSetRegOrdDescLookup()
-        {
-            // Arrange
-            const string path = "TestData/keto-cta-quant-and-semi-quant.csv";
-            var goldMiner = new GoldMiner(path);
-
-            const string title = "DNcpv vs. OrdDesc";
-            var selector = new CreateSelector(title);
-            var selResult = goldMiner.Eta.Select(selector.Selector);
-
-            // Act 1
-            Assert.Equal(Token.OrdDesc, selector.RegressorCompile.token);
-            Assert.NotEqual(Token.OrdDesc, selector.DependentCompile.token);
-            Assert.NotEqual(Token.OrdAsc, selector.DependentCompile.token);
-            Assert.Equal(Token.Element, selector.DependentCompile.token);
-
-            var enumerable = selResult as (string id, double x, double y)[] ?? selResult.ToArray();
-            var valueTuples = selResult as (string id, double x, double y)[] ?? enumerable.ToArray();
-
-            Assert.NotEqual(0, valueTuples[0].y);
-
-            var newTuples = valueTuples.OrderBy(t => t.y)
-                .Select((t, index) => (t.id, x: (double)(index + 1), t.y))
-                .ToArray();
-
-            foreach (var (id, x, y) in valueTuples)
-            {
-                testOutputHelper.WriteLine($"sel- Element ID: {id}, X: {x}, Y: {y}");
-            }
-            testOutputHelper.WriteLine("");
-            foreach (var (id, x, y) in newTuples)
-            {
-                testOutputHelper.WriteLine($"new- Element ID: {id}, X: {x}, Y: {y}");
-            }
-
-            Assert.NotEqual(0, newTuples[^1].y);
-            Assert.Equal(6.3, newTuples[^1].y, 0.300);
-
-            for (var x = 0; x < newTuples.Length - 1; x++) Assert.Equal(x + 1, newTuples[x].x);
-
-            var xyList = newTuples.Select(tuple => (tuple.x, tuple.y)).ToList();
-
-            // Assert
-            var regression = new RegressionPvalue(xyList);
-
-            // Act 2
-            Assert.NotNull(selResult);
-            Assert.NotEmpty(xyList);
-            Assert.NotNull(regression);
-            Assert.Equal(0.3, regression.PValue, 0.32);
-            testOutputHelper.WriteLine($"Regression: {regression}");
-        }
-
-        [Fact]
-        public void ConvertOrdinalTest()
-        {
-            // Arrange
-            const string path = "TestData/keto-cta-quant-and-semi-quant.csv";
-            var goldMiner = new GoldMiner(path);
-
-            const string title = "DNcpv vs. OrdDesc";
-            var selector = new CreateSelector(title);
-            var cor = new ConvertOrdinal(selector, goldMiner.Eta);
-
-            // Act 1
-            Assert.True(cor.IsRegOrd);
-            Assert.False(cor.IsDepOrd);
-            Assert.False(cor.IsAsc);
-
-
-            foreach (var (id, x, y) in cor.RawDataPoints)
-            {
-                testOutputHelper.WriteLine($"sel- Element ID: {id}, X: {x}, Y: {y}");
-            }
-
-            testOutputHelper.WriteLine("");
-            foreach (var (id, x, y) in cor.DataPoints)
-            {
-                testOutputHelper.WriteLine($"new- Element ID: {id}, X: {x}, Y: {y}");
-            }
-
-            Assert.NotEqual(0, cor.DataPoints[^1].y);
-            Assert.Equal(6.3, cor.DataPoints[^1].y, 0.300);
-
-            for (var x = 0; x < cor.DataPoints.Length - 1; x++) Assert.Equal(x + 1, cor.DataPoints[x].x);
-
-            // Assert
-            var regression = new RegressionPvalue(cor.DataPoints.ToList());
-
-            // Act 2
-            Assert.NotNull(selector);
-            Assert.NotNull(regression);
-            Assert.Equal(0.3, regression.PValue, 0.32);
-            testOutputHelper.WriteLine($"Regression: {regression}");
-        }
-
-
     }
-
 }
